fd19266f2c72960b39008dbf5d87766e94c3aa71
Divya R <kwrdivyaraj100@gmail.com>
2024-03-19 15:33:46 +0530

    Adding getAllUsers route handler to fetch all users from the database

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const userRouter = require("./routes/user_routes");
 dotenv.config();
 
 const app = express();
 
+app.use("/user", userRouter);
 
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.gia1v3o.mongodb.net/movie_booking?retryWrites=true&w=majority&appName=Cluster0`)
     .then(() => app.listen(5000))
