8d7a6997e8e9e775d22fb45a79701def4443e1b2
Divya R <kwrdivyaraj100@gmail.com>
2024-03-19 11:08:14 +0530

    Adding basic Express server setup with a route

---
+++ backend/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express()
+
+app.use("/", (req, res, next) => {
+    res.send("Hi");
+});
+
+app.listen(5000, () => {
+    console.log(`Connected To Localhost ${5000}`);
+});
