751645cfdff7cf567c720f41c15a944bb1b2730c
Divya R <kwrdivyaraj100@gmail.com>
2024-03-21 14:20:28 +0530

    Adding admin

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRouter = require("./routes/user_routes");
+const adminRouter = require('./routes/admin_routes');
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
 app.use("/user", userRouter);
+app.use("/admin", adminRouter)
 
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.gia1v3o.mongodb.net/movie_booking?retryWrites=true&w=majority&appName=Cluster0`)
     .then(() => app.listen(5000))
