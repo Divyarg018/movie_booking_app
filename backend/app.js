c6f191df8def618a2ffff73ad466a312d974e779
Divya R <kwrdivyaraj100@gmail.com>
2024-03-21 22:14:52 +0530

    Adding newBooking route to bookingRouter

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 const userRouter = require("./routes/user_routes.js");
 const adminRouter = require('./routes/admin_routes.js');
 const movieRouter = require('./routes/movie_routes.js');
+const bookingsRouter = require('./routes/booking_routes.js');
 dotenv.config();
 
 const app = express();
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use("/user", userRouter);
 app.use("/admin", adminRouter);
 app.use("/movie", movieRouter);
+app.use("/booking", bookingsRouter);
 
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.gia1v3o.mongodb.net/movie_booking?retryWrites=true&w=majority&appName=Cluster0`)
     .then(() => app.listen(5000))
